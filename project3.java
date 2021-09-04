a8675517a6c01432cb484ab174cee1faa1040af0
la-lith12 <lalithashwin98@gmail.com>
2021-09-04 14:55:03 +0530

    prog for variable declaration

--- project3.java
+++ project3.java
@@ -1,1 +1,16 @@
+package java;
+
+public class datatype{
+	
+	public static void main(string[] args) {
+		
+		int a = 18;
+		char b ='s';
+		byte c= 04;
+		double d = 04.18;
+		float e= 01.4i;
+		boolean f= true;
+		System.out.println(a " " b " " c " " d " " e " " f " ")
+	}
+}
 

b14dd0b690b071f3c712eea8cf9ad9ff28de3a38
la-lith12 <lalithashwin98@gmail.com>
2021-09-04 15:18:03 +0530

    prog to check java

--- project5.java
+++ project5.java
@@ -1,1 +1,22 @@
+package java;
+
+public class datatype{
+	
+	public static void main(string[] args) {
+
+		System.out.println("\njava version: "+System.getProperty("java.version"));
+		System.out.println("Java runtime version: "+System.getProperty("java.runtime.version"));
+		System.getProperty("java.runtime.version");
+		System.out.println("Java Home "+System.getProperty("java.home"));
+		System.out.println("java vendor :" +System.getProperty("java.vendor"));
+		System.out.println("Java Vendor URL: "+System.getProperty('java.vendor.url"));
+		System.out.println("Java Class Path: "+System.getProperty("java.class.path")+\n");
+		
+		
+		
+		
+	}
+	
+
+}
 

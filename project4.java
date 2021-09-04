0f5ca071ef3e825fea01d38dd651f95dcb6ad873
la-lith12 <lalithashwin98@gmail.com>
2021-09-04 15:06:15 +0530

    prog for data types

--- project4.java
+++ project4.java
@@ -1,1 +1,21 @@
+package java;
 
+public class datatype{
+	
+	public static void main(string[] args) {
+
+		System.out.println("size of byte :" +(byte.size/8) + " bytes.");
+		System.out.println("size of int :" +(byte.size/8) + " bytes.");
+		System.out.println("size of short :" +(byte.size/8) + " bytes.");
+		System.out.println("size of long :" +(byte.size/8) + " bytes.");
+		System.out.println("size of char :" +(byte.size/8) + " bytes.");
+		System.out.println("size of float :" +(byte.size/8) + " bytes.");
+		System.out.println("size of double :" +(byte.size/8) + " bytes.");
+		
+		
+		
+		
+	}
+	
+
+}

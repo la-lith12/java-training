9b42a321a60a9a0c001e30a3b51870f165e62ed9
la-lith12 <88979624+la-lith12@users.noreply.github.com>
2021-09-11 17:00:10 +0530

    converting different data type

---
+++ convert.java
@@ -0,0 +1,17 @@
+package java;
+import java.util.Scanner;
+public class convert {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Enter a number :");
+		Scanner scanner = new Scanner(System.in);
+		int a = scanner.nextInt();
+		int b = scanner.nextInt();
+		
+		scanner.close();
+		
+
+	}
+
+}

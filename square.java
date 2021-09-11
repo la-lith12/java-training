f19bbd575f5318dccee654f6dfe886c3540f870a
la-lith12 <88979624+la-lith12@users.noreply.github.com>
2021-09-11 15:50:37 +0530

    square and square-root

---
+++ square.java
@@ -0,0 +1,37 @@
+package java;
+import java.util.Scanner;
+
+class square
+{
+	public static double sqrt(int a) {
+		double temp;
+		double str = a/2;
+		
+		do {
+			
+			temp = str;
+			str =(temp +(a/temp))/2;
+			
+		}while((temp-str)!=0);
+		
+		return str;	
+	}
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Enter a number to square:");
+		Scanner scanner = new Scanner(System.in);
+		int sq = scanner.nextInt();
+		scanner.close();
+		
+		System.out.println("The square of the given value is:" + sq*sq );
+		
+		
+		System.out.println("Enter a number:");
+		Scanner scan = new Scanner(System.in);
+		int num = scan.nextInt();
+		scan.close();
+		
+		System.out.println("Square root of "+ num + " is : " + sqrt(num));
+	}
+}

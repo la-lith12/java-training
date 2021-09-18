c2266ef5f5bf3d597fd344a81f6cc64761b5dbf4
la-lith12 <88979624+la-lith12@users.noreply.github.com>
2021-09-18 18:31:25 +0530

    Add files via upload

---
+++ OUTPUT_6.java
@@ -0,0 +1,16 @@
+class Kcg{
+   static int i = 10;
+  static String y = "KCG College of Technology";
+  
+   public static void main(String a[]) 
+   {
+       System.out.println("i:"+i);
+       System.out.println("s:"+y);
+   }
+}
+
+/* output :
+	 
+	i:10
+	s:KCG College of Technology
+	*/
